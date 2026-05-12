67b7b1f4ac9312ba9056949282e0e52110ae1d3e
Hornflakes <cristianpetrugug@gmail.com>
2026-05-12 21:48:09 +0300

    feat: mishearing the song

---
+++ mysql-database/mysql-database.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/users?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	{
+		query := `
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at DATETIME,
+                PRIMARY KEY (id)
+            );`
+
+		if _, err := db.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	var id int64
+	{
+		username := "johnny"
+		password := "begoode"
+		createdAt := time.Now()
+
+		res, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err = res.LastInsertId()
+		fmt.Println(id)
+	}
+
+	{
+		type user struct {
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		}
+
+		rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		var users []user
+		for rows.Next() {
+			var u user
+
+			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+			if err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, u)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%#v", users)
+	}
+
+	{
+		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
